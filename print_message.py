28005152cf177fc5dd1db6aef67440c61e0d6fb6
Edson Nogueira <edson.nogueira@artefact.com>
2024-10-24 21:33:36 -0300

    Test hello with actions.

--- print_message.py
+++ print_message.py
@@ -1,4 +1,6 @@
 import os 
 
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print('message=Hello, world!', file=fh)
+if __name__ == "__main__":
+
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        print('message=Hello, world!', file=fh)
